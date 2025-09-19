3d96a025952b395a7758c44d2f6f9013a53ca25e
Siyabonga Khanyile <145035815+siyabongamasiya@users.noreply.github.com>
2025-09-19 14:53:07 +0200

    Update index.js
    added import of cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const cors = require("cors");
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
